f867c8d4277f73a2c81dab9cc3a5e343fe8b3f77
Nannapas Manoman <66070104@kmitl.ac.th>
2026-06-07 21:30:17 +0700

    update backend url

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,7 @@ function Login() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Welcome</h1>
-      <a href="http://localhost:8000/auth/login">
+      <a href="https://test1-8n5x.onrender.com/auth/login">
         <button>Sign in with Google</button>
       </a>
     </div>
